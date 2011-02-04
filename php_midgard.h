8d879258b6c558ddf475feb6f37b03630d72bfaf
piotras <piotrek.pokora@gmail.com>
2011-02-04 15:28:52 +0100

    Added missed midgard2_g_mainloop

--- php_midgard.h
+++ php_midgard.h
@@ -212,6 +212,7 @@ PHP_MINIT_FUNCTION(midgard2_key_config);
 PHP_MINIT_FUNCTION(midgard2_key_config_file);
 PHP_MINIT_FUNCTION(midgard2_reflection_workaround);
 PHP_MINIT_FUNCTION(midgard2_query);
+PHP_MINIT_FUNCTION(midgard2_g_mainloop);
 
 zend_class_entry *php_midgard_get_baseclass_ptr(zend_class_entry *ce);
 zend_class_entry *php_midgard_get_baseclass_ptr_by_name(const char *name TSRMLS_DC);
