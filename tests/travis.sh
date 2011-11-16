61104e4bed60e938b2bc6af3bfdcff48170003ed
Henri Bergius <henri.bergius@iki.fi>
2011-11-16 14:07:12 +0200

    We need to enable_dl in php.ini

--- tests/travis.sh
+++ tests/travis.sh
@@ -13,3 +13,7 @@ wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/x
 sudo dpkg -i --force-depends libmidgard2-2010_10.05.5.1-1_i386.deb  
 sudo dpkg -i midgard2-common_10.05.5.1-1_i386.deb
 sudo dpkg -i libmidgard2-dev_10.05.5.1-1_i386.deb
+
+# Enable dynamic loading of extensions in PHP
+# FIXME: get php.ini path in a non-hardcoded way
+echo "enable_dl=On" >> ~/.phpfarm/inst/php-5.3.8/lib/php.ini
