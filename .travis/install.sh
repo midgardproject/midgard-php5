876f46b33894608c2c36bdb9c628b2391ac514c5
Henri Bergius <henri.bergius@iki.fi>
2011-11-16 14:38:21 +0200

    Build and install extension before testing

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,6 @@
 # Install Pake
 pyrus channel-discover pear.indeyets.ru
 pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
-echo "Pake is in `pyrus get bin_dir|tail -1`"
 
 # Install Midgard from OBS
 sudo apt-get install -y dbus libgda-4.0-4 libgda-4.0-dev
@@ -13,6 +12,9 @@ wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/x
 sudo dpkg -i --force-depends libmidgard2-2010_10.05.5.1-1_i386.deb  
 sudo dpkg -i midgard2-common_10.05.5.1-1_i386.deb
 sudo dpkg -i libmidgard2-dev_10.05.5.1-1_i386.deb
+
+# Build and install the Midgard2 extension
+php `pyrus get php_dir|tail -1`/pake.php install
 
 # Enable dynamic loading of extensions in PHP
 echo "enable_dl=On" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
