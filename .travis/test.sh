ee625ddbed981244436909739b1ba1db720929fb
piotras <piotrek.pokora@gmail.com>
2011-12-20 15:59:19 +0100

    Removed valgrind

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-valgrind -v --log-file=segfault.log php -r "dl('midgard2.so'); print_r(new midgard_query_storage('midgard_person'));"
-#valgrind -v --log-file=segfault.log php `pyrus get php_dir|tail -1`/pake.php test
-cat segfault.log
+php `pyrus get php_dir|tail -1`/pake.php test
