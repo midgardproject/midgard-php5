481d60046adc3b2bce42f249556ab78deb19cf59
piotras <piotrek.pokora@gmail.com>
2012-01-11 08:41:06 +0100

    Build core from git

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,14 +4,18 @@
 pyrus channel-discover pear.indeyets.ru
 pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
 
-# Install Midgard from OBS
-sudo apt-get install -y dbus libgda-4.0-4 libgda-4.0-dev
-wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/libmidgard2-2010_10.05.5.1-1_i386.deb 
-wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/midgard2-common_10.05.5.1-1_i386.deb
-wget http://download.opensuse.org/repositories/home:/midgardproject:/ratatoskr/xUbuntu_10.04/i386/libmidgard2-dev_10.05.5.1-1_i386.deb 
-sudo dpkg -i --force-depends libmidgard2-2010_10.05.5.1-1_i386.deb  
-sudo dpkg -i midgard2-common_10.05.5.1-1_i386.deb
-sudo dpkg -i libmidgard2-dev_10.05.5.1-1_i386.deb
+# Options
+MIDGARD_LIBS_VERSION=10.05.5.1-1
+MIDGARD_EXT_VERSION=ratatoskr
+
+# Install Midgard2 library dependencies from OBS
+sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev valgrind
+
+# Build Midgard2 core from recent tarball
+wget -q https://github.com/midgardproject/midgard-core/tarball/${MIDGARD_EXT_VERSION}
+tar -xzf ratatoskr
+sh -c "cd midgardproject-midgard-core-*&&./autogen.sh --prefix=/usr; make; sudo make install"
+rm -f ${MIDGARD_EXT_VERSION}
 
 # Build and install the Midgard2 extension
 php `pyrus get php_dir|tail -1`/pake.php install
