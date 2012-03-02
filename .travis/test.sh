8afad61c8b780045733db48e1b7983827e5b5901
piotras <piotrek.pokora@gmail.com>
2012-03-02 11:55:39 +0100

    Try pear full path

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,6 +2,6 @@
 php `pyrus get php_dir|tail -1`/pake.php test
 
 # PHPUnit tests
-sudo pear channel-discover pear.phpunit.de
-sudo pear install phpunit/PHPUnit
+sudo /usr/bin/pear channel-discover pear.phpunit.de
+sudo /usr/bin/pear install phpunit/PHPUnit
 sh -c "cd tests/phpunit&&php -c midgard2.ini /usr/bin/phpunit -c phpunit_sqlite.xml.dist"
