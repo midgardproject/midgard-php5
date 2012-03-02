0917abd21ac3d96cf378e3fbbc86c01306cee3d0
piotras <piotrek.pokora@gmail.com>
2012-03-02 11:04:59 +0100

    Run phpunit tests

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 php `pyrus get php_dir|tail -1`/pake.php test
+
+# PHPUnit tests
+sudo pear channel-discover pear.phpunit.de
+sudo pear install phpunit/PHPUnit
+sh -c "cd tests/phpunit&&php -c midgard2.ini /usr/bin/phpunit -c phpunit_sqlite.xml.dist"
