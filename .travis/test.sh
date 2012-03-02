54bb32e5614dda711e106ea09c2330a1fbba4b55
piotras <piotrek.pokora@gmail.com>
2012-03-02 14:26:56 +0100

    Run phpunit tests, then pake tasks

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-php `pyrus get php_dir|tail -1`/pake.php test
 
 # PHPUnit tests
 sudo /usr/bin/pear install -f pear
 sudo /usr/bin/pear channel-discover pear.phpunit.de
 sudo /usr/bin/pear channel-discover pear.symfony-project.com
 sudo /usr/bin/pear install --alldeps phpunit/PHPUnit
-sh -c "cd tests/phpunit&&ls"
-sh -c "ls tests/phpunit"
 sh -c "cd tests/phpunit&&php -c midgard2.ini /usr/bin/phpunit -c phpunit_sqlite.xml.dist"
+
+php `pyrus get php_dir|tail -1`/pake.php test
