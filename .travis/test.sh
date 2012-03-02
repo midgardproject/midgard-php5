aaf3d1ad13b7bc5d9277f70db005d04859044299
piotras <piotrek.pokora@gmail.com>
2012-03-02 12:31:36 +0100

    Install phpunit dependencies

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,5 +4,6 @@ php `pyrus get php_dir|tail -1`/pake.php test
 # PHPUnit tests
 sudo /usr/bin/pear install -f pear
 sudo /usr/bin/pear channel-discover pear.phpunit.de
-sudo /usr/bin/pear install phpunit/PHPUnit
+sudo /usr/bin/pear channel-discover pear.symfony-project.com
+sudo /usr/bin/pear install --alldeps phpunit/PHPUnit
 sh -c "cd tests/phpunit&&php -c midgard2.ini /usr/bin/phpunit -c phpunit_sqlite.xml.dist"
